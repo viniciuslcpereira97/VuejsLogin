04816cd93343c7f58b7f6a2162ce9915475cb833
Vinicius Luiz <viniciuslcp97@gmail.com>
2018-04-11 10:56:12 -0300

    facebook login component

---
+++ src/components/FacebookLogin.vue
@@ -0,0 +1,36 @@
+<template lang="html">
+
+  <vue-facebook-login class="button"
+    appId="157805414887234"
+    @login="getUserData"
+    @logout="onLogout"
+    @get-initial-status="getUserData">
+  </vue-facebook-login>
+
+</template>
+
+<script>
+
+import VueFacebookLogin from 'facebook-login-vuejs';
+
+export default {
+  name: 'FacebookLogin',
+  components: {
+    VueFacebookLogin,
+  },
+
+  methods: {
+    getUserData(response) {
+      console.log(response);
+    },
+    onLogout(response) {
+      console.log(response);
+    },
+  },
+
+};
+
+</script>
+
+<style lang="css">
+</style>
